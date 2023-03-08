ec15defab03866eb799c1284b3773a5648cdbd4b
guzhihao <936285702@qq.com>
2023-03-08 13:57:18 +0800

    feat: 阴影

--- my-demo/src/main/main.js
+++ my-demo/src/main/main.js
@@ -10,6 +10,51 @@ import { AxesHelper } from 'three';
  */
 var scene = new THREE.Scene();
 
+/* 阴影 start */
+var geometry = new THREE.BoxGeometry(40, 100, 40);
+var material = new THREE.MeshLambertMaterial({
+  color: 0x0000ff,
+});
+var mesh = new THREE.Mesh(geometry, material);
+// mesh.position.set(0,0,0)
+scene.add(mesh);
+
+// 设置产生投影的网格模型
+mesh.castShadow = true;
+
+//创建一个平面几何体作为投影面
+var planeGeometry = new THREE.PlaneGeometry(300, 200);
+var planeMaterial = new THREE.MeshLambertMaterial({
+  color: 0x999999,
+});
+// 平面网格模型作为投影面
+var planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh); //网格模型添加到场景中
+planeMesh.rotateX(-Math.PI / 2); //旋转网格模型
+planeMesh.position.y = -50; //设置网格模型y坐标
+// 设置接收阴影的投影面
+planeMesh.receiveShadow = true;
+
+// 方向光
+var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+// 设置光源位置
+directionalLight.position.set(60, 100, 40);
+scene.add(directionalLight);
+// 设置用于计算阴影的光源对象
+directionalLight.castShadow = true;
+// 设置计算阴影的区域，最好刚好紧密包围在对象周围
+// 计算阴影的区域过大：模糊  过小：看不到或显示不完整
+directionalLight.shadow.camera.near = 0.5;
+directionalLight.shadow.camera.far = 300;
+directionalLight.shadow.camera.left = -50;
+directionalLight.shadow.camera.right = 50;
+directionalLight.shadow.camera.top = 200;
+directionalLight.shadow.camera.bottom = -100;
+// 设置mapSize属性可以使阴影更清晰，不那么模糊
+// directionalLight.shadow.mapSize.set(1024,1024)
+console.log(directionalLight.shadow.camera);
+/* 阴影 end */
+
 /**
  * 相机设置
  */
